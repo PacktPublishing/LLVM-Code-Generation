7c94fbd73b894b4edaf73e8cbb4836207c6d2201
Melika Norouzbeygi <melikanorouzbeygi75@gmail.com>
2025-08-07 16:00:14 -0600

    add: add my first machineir

--- ch3/machineir/your_turn/populate_function.cpp
+++ ch3/machineir/your_turn/populate_function.cpp
@@ -32,7 +32,8 @@ using namespace llvm;
 //
 // The local variable named var is expected to live on the stack.
 MachineFunction *populateMachineIR(MachineModuleInfo &MMI, Function &Foo,
-                                   Register W0, Register W1) {
+                                   Register W0, Register W1)
+{
   MachineFunction &MF = MMI.getOrCreateMachineFunction(Foo);
 
   // The type for bool.
@@ -52,5 +53,72 @@ MachineFunction *populateMachineIR(MachineModuleInfo &MMI, Function &Foo,
                                                        /*IsSpillSlot=*/false);
 
   // TODO: Populate MF.
+
+  // building basic blocks
+  MachineBasicBlock *BB1 = MF.CreateMachineBasicBlock();
+  MF.push_back(BB1);
+  MachineBasicBlock *BB2 = MF.CreateMachineBasicBlock();
+  MF.push_back(BB2);
+  MachineBasicBlock *BB3 = MF.CreateMachineBasicBlock();
+  MF.push_back(BB3);
+
+  // connecting basic blocks
+  BB1->addSuccessor(BB2);
+  BB1->addSuccessor(BB3);
+  BB2->addSuccessor(BB3);
+
+  // populating bb1
+  MachineIRBuilder builder(*BB1, BB1->end());
+
+  Register A = builder.buildCopy(I32, W0).getReg(0);
+  Register B = builder.buildCopy(I32, W1).getReg(0);
+
+  Register VarAddr = builder.buildFrameIndex(VarAddrLLT, FrameIndex).getReg(0);
+
+  Register Add = builder.buildAdd(I32, A, B).getReg(0);
+
+  builder.buildStore(Add, VarAddr, PtrInfo, VarStackAlign);
+
+  Register Zero = builder.buildConstant(I32, 0).getReg(0);
+  Register Var = builder.buildLoad(I32, VarAddr, PtrInfo, VarStackAlign).getReg(0);
+
+  Register Cmp = builder.buildICmp(CmpInst::ICMP_EQ, I1, Zero, Var).getReg(0);
+
+  builder.buildBrCond(Cmp, *BB2);
+  builder.buildBr(*BB3);
+
+  // populating bb2
+  builder.setInsertPt(*BB2, BB2->end());
+  Var = builder.buildLoad(I32, VarAddr, PtrInfo, VarStackAlign).getReg(0);
+  builder.buildCopy(W0, Var);
+  builder.buildInstr(TargetOpcode::INLINEASM, {}, {})
+      .addExternalSymbol("bl @bar")
+      .addImm(0)
+      .addReg(W0, RegState::Implicit);
+
+  builder.buildInstr(TargetOpcode::INLINEASM, {}, {})
+      .addExternalSymbol("bl @baz")
+      .addImm(0)
+      .addReg(W0, RegState::Implicit | RegState::Define);
+
+  Register BazRes = builder.buildCopy(I32, W0).getReg(0);
+
+  builder.buildStore(BazRes, VarAddr, PtrInfo, VarStackAlign);
+
+  builder.buildBr(*BB3);
+
+  // populating bb3
+  builder.setInsertPt(*BB3, BB3->end());
+  Var = builder.buildLoad(I32, VarAddr, PtrInfo, VarStackAlign).getReg(0);
+  builder.buildCopy(W0, Var);
+  builder.buildInstr(TargetOpcode::INLINEASM, {}, {})
+      .addExternalSymbol("bl @bar")
+      .addImm(0)
+      .addReg(W0, RegState::Implicit);
+
+  builder.buildInstr(TargetOpcode::INLINEASM, {}, {})
+      .addExternalSymbol("ret")
+      .addImm(0);
+
   return &MF;
 }
